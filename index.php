75be3202af146b69852aae9b9445379b3b5c7430
Mohamad Rizkon Nurkholis <rizkon86system@gmail.com>
2023-08-29 23:22:12 +0700

    perbaikan font

--- index.php
+++ index.php
@@ -26,14 +26,14 @@ if (isset($_GET['link_page'])) {
         <div class="container">
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class=" container-fluid">
-                    <a class="navbar-brand" href="#">Pendaftaran Beasiswa</a>
+                    <a class="navbar-brand fw-bold text-primary" href="index.php">Pendaftaran Beasiswa</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav ms-auto">
                             <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                                <a class="nav-link active fw-bold text-primary" aria-current="page" href="index.php">Home</a>
                             </li>
                         </ul>
                     </div>
@@ -58,43 +58,42 @@ if (isset($_GET['link_page'])) {
             <div class="tab-content" id="pills-tabContent">
                 <div class="tab-pane fade <?= SetContentPage("1", $link_page); ?>" id="beasiswa" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
                     <div class="section-menu">
-                        <h4>Jenis Beasiswa</h4>
                         <p>Beasiswa berdasarkan bidang studi atau jurusan studi adalah sumber pendanaan yang diberikan kepada mahasiswa yang memilih untuk mengejar gelar dalam bidang studi tertentu yang dianggap relevan dengan misi atau tujuan pendanaan yang ditetapkan oleh pemberi beasiswa, dengan harapan bahwa mereka akan menjadi kontributor berpengaruh dalam bidang tersebut.</p>
                         <ol>
                             <li>
                                 <h5>Beasiswa Akademik</h5>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati repellat, autem possimus accusamus sed maiores, culpa molestias est incidunt reiciendis voluptatum at debitis unde quidem temporibus deserunt distinctio voluptatibus? Nemo.</p>
-                                <p>Adapun persyaratan yang dibutuhkan adalah</p>
+                                <p>Beasiswa akademik merupakan bantuan finansial yang diberikan kepada mahasiswa yang telah menunjukkan prestasi akademik yang luar biasa. Ini adalah peluang untuk mahasiswa yang berdedikasi dan berprestasi tinggi dalam studi mereka. Beasiswa ini ditujukan untuk membantu mahasiswa mencapai potensi penuh mereka dalam pendidikan.</p>
+                                <p>Persyaratan untuk mendapatkan beasiswa ini adalah:</p>
                                 <ul>
                                     <li>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eius. Dignissimos esse magnam a, beatae ratione quidem tempora sapiente voluptate quibusdam nisi exercitationem maiores perspiciatis error ea impedit, ut inventore?</p>
+                                        <p>Prestasi akademik yang sangat baik selama kuliah.</p>
                                     </li>
                                     <li>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eius. Dignissimos esse magnam a, beatae ratione quidem tempora sapiente voluptate quibusdam nisi exercitationem maiores perspiciatis error ea impedit, ut inventore?</p>
+                                        <p>Motivasi dan komitmen untuk mengejar keunggulan akademik.</p>
                                     </li>
                                     <li>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eius. Dignissimos esse magnam a, beatae ratione quidem tempora sapiente voluptate quibusdam nisi exercitationem maiores perspiciatis error ea impedit, ut inventore?</p>
+                                        <p>Kepemimpinan dan partisipasi dalam kegiatan akademik ekstrakurikuler.</p>
                                     </li>
                                 </ul>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit asperiores rem vero, labore libero unde vel culpa fuga ea ex!</p>
+                                <p>Ini adalah kesempatan langka untuk mendapatkan bantuan finansial yang dapat membantu mewujudkan impian akademik Anda. Jangan lewatkan kesempatan ini!</p>
                                 <a class="btn btn-primary mb-4" href="index.php?link_page=2&jenis_beasiswa=akademik">Daftar Sekarang</a>
                             </li>
                             <li>
                                 <h5>Beasiswa Non Akademik</h5>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati repellat, autem possimus accusamus sed maiores, culpa molestias est incidunt reiciendis voluptatum at debitis unde quidem temporibus deserunt distinctio voluptatibus? Nemo.</p>
-                                <p>Adapun persyaratan yang dibutuhkan adalah</p>
+                                <p>Beasiswa non-akademik adalah bentuk dukungan finansial yang diberikan kepada mahasiswa yang telah menunjukkan keahlian dan prestasi di luar lingkup akademik. Ini adalah kesempatan bagi mereka yang berdedikasi dalam kegiatan ekstrakurikuler, sukarela, atau memiliki bakat khusus.</p>
+                                <p>Persyaratan untuk mendapatkan beasiswa ini adalah:</p>
                                 <ul>
                                     <li>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eius. Dignissimos esse magnam a, beatae ratione quidem tempora sapiente voluptate quibusdam nisi exercitationem maiores perspiciatis error ea impedit, ut inventore?</p>
+                                        <p>Pengalaman dan prestasi yang signifikan dalam kegiatan non-akademik.</p>
                                     </li>
                                     <li>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eius. Dignissimos esse magnam a, beatae ratione quidem tempora sapiente voluptate quibusdam nisi exercitationem maiores perspiciatis error ea impedit, ut inventore?</p>
+                                        <p>Komitmen untuk berkontribusi positif pada komunitas atau masyarakat.</p>
                                     </li>
                                     <li>
-                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, eius. Dignissimos esse magnam a, beatae ratione quidem tempora sapiente voluptate quibusdam nisi exercitationem maiores perspiciatis error ea impedit, ut inventore?</p>
+                                        <p>Bakat khusus atau prestasi dalam bidang tertentu.</p>
                                     </li>
                                 </ul>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit asperiores rem vero, labore libero unde vel culpa fuga ea ex!</p>
+                                <p>Ini adalah kesempatan unik untuk mendapatkan dukungan finansial untuk pengembangan bakat dan minat Anda di luar akademik. Jangan lewatkan kesempatan ini!</p>
                                 <a class="btn btn-primary mb-4" href="index.php?link_page=2&jenis_beasiswa=non_akademik">Daftar Sekarang</a>
                             </li>
                         </ol>
@@ -183,45 +182,45 @@ if (isset($_GET['link_page'])) {
 
                 <div class="tab-pane fade <?= SetContentPage("3", $link_page); ?>" id="hasil" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">
                     <div class="section-menu">
-                        <h4>List Pendaftar</h4>
+                        <h4 class="fw-bold text-primary mb-4">List Pendaftar</h4>
 
                         <?php
                         while ($user_data = mysqli_fetch_array($result)) {
 
                         ?>
-                            <div class="row grid-item border">
+                            <div class="row grid-item border border-3">
                                 <div class="col-md-3 col-lg-4">
-                                    <img class="img-fluid" width="100px" src="uploads/<?= $user_data['berkas']; ?>" alt="">
+                                    <img class="img-fluid mt-4" width="140px" src="uploads/<?= $user_data['berkas']; ?>" alt="">
                                 </div>
                                 <div class="col-md-9 col-lg-8">
                                     <div class="row">
-                                        <div class="col-sm-6 col-md-6 col-lg-4 mb-10">
-                                            <h4 class="fs-5">Nama:</h4>
-                                            <h5 class="fs-6"><?= $user_data['nama']; ?></h5>
+                                        <div class="col-sm-6 col-md-6 col-lg-4 mt-4">
+                                            <h4 class="fs-5 fw-normal">Nama:</h4>
+                                            <h5 class="fs-6 font-monospace"><?= $user_data['nama']; ?></h5>
                                         </div>
-                                        <div class="col-sm-6 col-md-6 col-lg-4">
-                                            <h4 class="fs-5">Email:</h4>
-                                            <h5 class="fs-6"><?= $user_data['email']; ?></h5>
+                                        <div class="col-sm-6 col-md-6 col-lg-4 mt-4">
+                                            <h4 class="fs-5 fw-normal">Email:</h4>
+                                            <h5 class="fs-6 font-monospace"><?= $user_data['email']; ?></h5>
                                         </div>
-                                        <div class="col-sm-6 col-md-6 col-lg-4">
-                                            <h4 class="fs-5">No Handphone:</h4>
-                                            <h5 class="fs-6"><?= $user_data['hp']; ?></h5>
+                                        <div class="col-sm-6 col-md-6 col-lg-4 mt-4">
+                                            <h4 class="fs-5 fw-normal">No Handphone:</h4>
+                                            <h5 class="fs-6 font-monospace"><?= $user_data['hp']; ?></h5>
                                         </div>
-                                        <div class="col-sm-6 col-md-6 col-lg-4">
-                                            <h4 class="fs-5">Semester:</h4>
-                                            <h5 class="fs-6"><?= $user_data['semester']; ?></h5>
+                                        <div class="col-sm-6 col-md-6 col-lg-4 mt-4">
+                                            <h4 class="fs-5 fw-normal">Semester:</h4>
+                                            <h5 class="fs-6 font-monospace"><?= $user_data['semester']; ?></h5>
                                         </div>
-                                        <div class="col-sm-6 col-md-6 col-lg-4">
-                                            <h4 class="fs-5">IPK:</h4>
-                                            <h5 class="fs-6"><?= $user_data['ipk']; ?></h5>
+                                        <div class="col-sm-6 col-md-6 col-lg-4 mt-4">
+                                            <h4 class="fs-5 fw-normal">IPK:</h4>
+                                            <h5 class="fs-6 font-monospace"><?= $user_data['ipk']; ?></h5>
                                         </div>
-                                        <div class="col-sm-6 col-md-6 col-lg-4">
-                                            <h4 class="fs-5">Beasiswa:</h4>
-                                            <h5 class="fs-6"><?= $user_data['beasiswa']; ?></h5>
+                                        <div class="col-sm-6 col-md-6 col-lg-4 mt-4">
+                                            <h4 class="fs-5 fw-normal">Beasiswa:</h4>
+                                            <h5 class="fs-6 font-monospace"><?= $user_data['beasiswa']; ?></h5>
                                         </div>
-                                        <div class="col-sm-6 col-md-6 col-lg-4">
-                                            <h4 class="fs-5">Status:</h4>
-                                            <h5 class="fs-6"><?= $user_data['status']; ?></h5>
+                                        <div class="col-sm-6 col-md-6 col-lg-4 mt-4">
+                                            <h4 class="fs-5 fw-normal">Status:</h4>
+                                            <h5 class="fs-6 font-monospace"><?= $user_data['status']; ?></h5>
                                         </div>
 
                                     </div>
